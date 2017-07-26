c6fe7918765cab193597ec9b071c5bfd4a482410
spagovir <edgarlin2000@gmail.com>
2017-07-26 15:20:40 -0700

    Now uses <div> instead of <textarea> for writing area, keyboard input bound to document instead of component, fixed printable character range detection.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      value: "awieojf",
+      value: "",
     };
     this.type = this.type.bind(this);
   }
   render() {
     return (
-        <div className = 'Paper'>
-          <textarea value={this.state.value} onKeyPress={this.type}/>
+        <div className="App">
+          <div className="Paper">
+            {this.state.value}
+          </div>
         </div>
     );
   }
   type(e) {
+    const ASCII_SPACE = 0x20, ASCII_DEL = 0x7f;
     var ch = e.which;
-    if(ch > 0x20 && ch != 0x7f)
+    if(ch >= ASCII_SPACE && ch != ASCII_DEL) // checks that ch is in printable range and not delete.
       this.setState({value: this.state.value + String.fromCharCode(ch)});
+    e.preventDefault();
+  }
+  componentWillMount() {
+    document.addEventListener("keypress", this.type, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keypress", this.type, false);
   }
 }
 

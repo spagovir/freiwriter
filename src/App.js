ec6bbfe4ab592ad87690c2f4443e645300d797a4
spagovir <edgarlin2000@gmail.com>
2018-02-15 15:40:28 -0800

    Typewriter no longer intercepts and types ctrl- shortcuts, making copying using ctrl-c possible on all browsers.

--- src/App.js
+++ src/App.js
@@ -21,28 +21,30 @@ class App extends Component {
   }
   type(e) {
 
-    const ASCII_BACKSPACE = 0x08, ASCII_DEL = 0x7f;
-    //character expanding function for linebreak formatting.
-    var expandChar = function(ch) {
-      const NEWLINE = '\r\n';
-      switch(ch) {
-        case '\r':
-          return NEWLINE;
-        case '\n':
-          return NEWLINE;
-        default:
-          return ch;
+    if(!e.ctrlKey) { //obviously, typewriter should not type/intercept ctrl shortcuts.
+      const ASCII_BACKSPACE = 0x08, ASCII_DEL = 0x7f;
+      //character expanding function for linebreak formatting.
+      var expandChar = function(ch) {
+        const NEWLINE = '\r\n';
+        switch(ch) {
+          case '\r':
+            return NEWLINE;
+          case '\n':
+            return NEWLINE;
+          default:
+            return ch;
+        }
       }
-    }
 
-    var ch = e.which;
-    if(ch != ASCII_BACKSPACE && ch != ASCII_DEL) { // checks that ch is in printable range and not delete.
-      this.setState((prevState, props) => ({value: prevState.value + expandChar(String.fromCharCode(ch))}));
-      clearTimeout(this.delay);
-      this.delay = setTimeout(this.curriedAutoType(0),1500);
+      var ch = e.which;
+      if(ch != ASCII_BACKSPACE && ch != ASCII_DEL) { // checks that ch is in printable range and not delete.
+        this.setState((prevState, props) => ({value: prevState.value + expandChar(String.fromCharCode(ch))}));
+        clearTimeout(this.delay);
+        this.delay = setTimeout(this.curriedAutoType(0),1500);
+      }
+      window.scrollTo(0, document.body.scrollHeight); //you can only type at the bottom anyway, so doc needs to be scrolled to bottom line whenever new text is typed there. 
+      e.preventDefault();//prevents the default action triggered by the keypress from occurring. I think this is only meant to stop people from accidentally going back to the previous page by pressing backspace, but i forgot to doc this when I first wrote this fml.
     }
-    window.scrollTo(0, document.body.scrollHeight);
-    e.preventDefault();//prevents the default action triggered by the keypress from occurring. I think this is only meant to stop people from accidentally going back to the previous page by pressing backspace, but i forgot to doc this when I first wrote this fml.
   }
   //returns a function callback that types one letter and registers the timer callback to type the next letter.
   curriedAutoType(index) {
